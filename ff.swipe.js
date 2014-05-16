e5b7c45db55162d84a715d9f7ccec7cdcf242861
Adrian Stride <a@mahi.cc>
2014-05-16 15:32:21 +0000

    Added Timeout

--- ff.swipe.js
+++ ff.swipe.js
@@ -13,27 +13,43 @@
 	};
 
 	$.ffSwipe.Defaults = {
-
+		Action : 'Init'
 	};
 
 	$.ffSwipe.prototype = {
 		Init : function(Opts) {
-			this.Opts = $.extend($.ffSwipe.Defaults,Opts);
-		
-			this.TouchStart = Touch ? 'touchstart' : 'mousedown';
-			this.TouchMove = Touch ? 'touchmove' : 'mousemove';
-			this.TouchEnd = Touch ? 'touchend' : 'mouseup';
-
-			this.Obj.bind(this.TouchStart,SwipeStart);
-			this.Obj.bind(this.TouchMove,SwipeMove);
-			this.Obj.bind(this.TouchEnd,SwipeEnd);
-
-			if (Touch){
-				this.Obj.bind('touchcancel',SwipeCancel);
-			} else {
-				this.Obj.bind('mouseleave',SwipeCancel);
+			if (typeof Opts === 'string') {
+				Opts = {Action:Opts};
 			}
 
+			this.Opts = $.extend($.ffSwipe.Defaults,Opts);
+		
+			if (this.Opts.Action === 'Stop') {
+				SwipeCancel();
+
+			} else if (this.Opts.Action === 'Destroy') {
+				this.Obj.unbind(this.TouchStart,SwipeStart);
+				this.Obj.unbind(this.TouchMove,SwipeMove);
+				this.Obj.unbind(this.TouchEnd,SwipeEnd);
+				this.Obj.unbind('touchcancel',SwipeCancel);
+				this.Obj.unbind('mouseleave',SwipeCancel);
+
+			} else if (this.Opts.Action === 'Init') {
+
+				this.TouchStart = Touch ? 'touchstart' : 'mousedown';
+				this.TouchMove = Touch ? 'touchmove' : 'mousemove';
+				this.TouchEnd = Touch ? 'touchend' : 'mouseup';
+
+				this.Obj.bind(this.TouchStart,SwipeStart);
+				this.Obj.bind(this.TouchMove,SwipeMove);
+				this.Obj.bind(this.TouchEnd,SwipeEnd);
+
+				if (Touch){
+					this.Obj.bind('touchcancel',SwipeCancel);
+				} else {
+					this.Obj.bind('mouseleave',SwipeCancel);
+				}
+			}
 		}
 	};
 
@@ -51,6 +67,8 @@
 	var Movement = 0;
 	var Direction = false;
 	var Obj = false;
+	var Event = false;
+	var Timer = false;
 
 	var Touch = false;
 	if ('ontouchstart' in window) {
@@ -60,6 +78,9 @@
 
 	function SwipeStart(e) {
 		e = e.originalEvent ? e.originalEvent : e;
+		Event = e;
+
+		Timer = setTimeout(SwipeCancel,3000);
 
 		Distance = 0;
 		Movement = 0;
@@ -95,7 +116,10 @@
 		}
 	}
 	function SwipeEnd(e) {
-		e = e.originalEvent;
+		if (Timer) {
+			clearTimeout(Timer);
+		}
+		e = e.originalEvent ? e.originalEvent : e;
 		if (Moving) {
 			if (Direction && Distance !== 0) {
 				Obj.trigger('swipe'+Direction,[Distance]);
@@ -112,11 +136,10 @@
 		}
 	}
 	function SwipeCancel(e) {
+		e = e ? e : Event;
 		SwipeEnd(e);
 	}
 	function SwipeLocation(Touch) {
-
-
 		if (Touch.clientY > (StartY+15) || Touch.clientY < (StartY-15)) {
 			return false;
 		}
